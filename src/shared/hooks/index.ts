653646a8e77941857f08d347631644f6ebd98f9d
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2026-03-10 17:52:02 -0300

    feat: ✨ add smooth scroll hook

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -3,4 +3,5 @@ export * from "./use-container-width.hook";
 export * from "./use-object-url.hook";
 export * from "./use-open-dialog.hook";
 export * from "./use-scroll-spy.hook";
+export * from "./use-smooth-scroll.hook";
 export * from "./use-tracking-query.hook";
---
+++ src/shared/hooks/use-smooth-scroll.hook.ts
@@ -0,0 +1,18 @@
+import { useCallback } from "react";
+
+export function useSmoothScroll() {
+  const scrollToSection = useCallback((sectionId: string) => {
+    const element = document.getElementById(sectionId);
+
+    if (!element) {
+      return;
+    }
+
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }, []);
+
+  return scrollToSection;
+}
